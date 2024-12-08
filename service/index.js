2f348c9847f1dbd75414bc7e5ef7ca5cd1132441
unknown <megan mckeehan>
2024-12-07 17:31:37 -0700

    switch to other computer

--- service/index.js
+++ service/index.js
@@ -1,81 +1,138 @@
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+// The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = {};
 let scores = [];
+let friendCodes = [];
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
-app.use(express.static('public'));
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-    const user = users[req.body.email];
-    if (user) {
-      res.status(409).send({ msg: 'Existing user' });
-    } else {
-      const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-      users[user.email] = user;
-  
+  const user = users[req.body.email];
+  if (user) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    let newUserFriendCode = Math.floor(Math.random() * 100000);
+    while (friendCodes.includes(newUserFriendCode)) {
+      newUserFriendCode = Math.floor(Math.random() * 100000);
+    }
+    const user = { email: req.body.email, password: req.body.password, token: uuid.v4(), friendCode: newUserFriendCode, friends: [], currentHighScore: 0 };
+    users[user.email] = user;
+
+    res.send({ token: user.token });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    if (req.body.password === user.password) {
+      user.token = uuid.v4();
       res.send({ token: user.token });
+      return;
     }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', (req, res) => {
+  const user = Object.values(users).find((u) => u.token === req.body.token);
+  if (user) {
+    delete user.token;
+  }
+  res.status(204).end();
+});
+
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
+});
+
+// SubmitScore
+apiRouter.post('/score', (req, res) => {
+  scores = updateScores(req.body, scores);
+  res.send(scores);
+});
+
+// GetFriendScores
+apiRouter.get('/friends/scores', (_req, res) => {
+    let friendScores = findFriendHighscores(req.body.email);
+    res.send(friendScores);
   });
   
-  apiRouter.post('/auth/login', async (req, res) => {
-    const user = users[req.body.email];
-    if (user) {
-      if (req.body.password === user.password) {
-        user.token = uuid.v4();
-        res.send({ token: user.token });
-        return;
-      }
-    }
-    res.status(401).send({ msg: 'Unauthorized' });
-  });
-  
-  apiRouter.delete('/auth/logout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-      delete user.token;
-    }
-    res.status(204).end();
-  });
-  
-  apiRouter.get('/scores', (_req, res) => {
+ // GetScores
+apiRouter.get('/friends/add', (_req, res) => {
     res.send(scores);
   });
   
-  apiRouter.post('/score', (req, res) => {
+  // SubmitScore
+  apiRouter.post('/friends/delete', (req, res) => {
     scores = updateScores(req.body, scores);
     res.send(scores);
   });
-  
-  
-  app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-  
-  
-  function updateScores(newScore, scores) {
-    let found = false;
-    for (const [i, prevScore] of scores.entries()) {
-      if (newScore.score > prevScore.score) {
-        scores.splice(i, 0, newScore);
-        found = true;
-        break;
-      }
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// updateScores considers a new score for inclusion in the high scores.
+function updateScores(newScore, scores) {
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore.score > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
     }
-  
-    if (!found) {
-      scores.push(newScore);
-    }
-  
-    if (scores.length > 10) {
-      scores.length = 10;
-    }
-  
-    return scores;
   }
+
+  if(users[newScore.email]) {
+    if (newScore.score > users[newScore.email].currentHighScore) {
+      users[newScore.email].currentHighScore = newScore.score;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  if (scores.length > 10) {
+    scores.length = 10;
+  }
+
+  return scores;
+}
+
+function findFriendHighscores(email){
+    let friendScores = []
+    const user = users[email];
+    user.friends.array.forEach(friendToken => {
+        let friend = users.find(e => e.friendCode === friendToken)
+        if (friend) {
+            friendScores.push({userName: friend.email, score: friend.currentHighScore});
+        }
+    });
+    return friendScores;
+}
+
